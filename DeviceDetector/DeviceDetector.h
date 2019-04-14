9abbf4d6b64366ae0b41efd1414be7a02457edb2
Alexander Zakatnov <me@zaakk.com>
2019-04-14 23:50:33 +0300

    pod start

---
+++ DeviceDetector/DeviceDetector.h
@@ -0,0 +1,19 @@
+//
+//  DeviceDetector.h
+//  DeviceDetector
+//
+//  Created by Alexander Zakatnov on 14/04/2019.
+//  Copyright © 2019 Alexander Zakatnov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceDetector.
+FOUNDATION_EXPORT double DeviceDetectorVersionNumber;
+
+//! Project version string for DeviceDetector.
+FOUNDATION_EXPORT const unsigned char DeviceDetectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceDetector/PublicHeader.h>
+
+
